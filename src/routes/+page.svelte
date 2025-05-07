86f813bf58b82a3a6b05fffaceef93bfe7a84f06
Swapnil Tripathi <swapnil06.st@gmail.com>
2025-05-08 00:23:21 +0530

    initialize

---
+++ src/routes/+page.svelte
@@ -0,0 +1,7 @@
+<svelte:head>
+	<title>Suforsa</title>
+</svelte:head>
+
+<div class="mx-auto flex min-h-screen flex-col items-center justify-center text-center">
+	<h1 class="font-heading text-[6vw] break-all select-none">SUFORSA</h1>
+</div>
---
+++ svelte.config.js
@@ -0,0 +1,25 @@
+import { mdsvex } from 'mdsvex';
+import adapter from '@sveltejs/adapter-static';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+
+const config = {
+	preprocess: [vitePreprocess(), mdsvex()],
+	kit: {
+		adapter: adapter({
+			pages: 'build',
+			assets: 'build',
+			fallback: null,
+			precompress: false,
+			strict: true
+		}),
+		paths: {
+			base: ''
+		},
+		prerender: {
+			entries: ['*']
+		}
+	},
+	extensions: ['.svelte', '.svx']
+};
+
+export default config;
