a41a895bba168eccb494fe58b804c3783597568e
Swapnil Tripathi <swapnil06.st@gmail.com>
2025-05-24 08:21:01 +0530

    add tm symbol

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,5 +3,5 @@
 </svelte:head>
 
 <div class="mx-auto flex min-h-screen flex-col items-center justify-center text-center">
-	<h1 class="font-heading text-[6vw] break-all select-none">SUFORSA</h1>
+	<h1 class="font-heading text-[10vw] select-none">SUFORSA&trade;</h1>
 </div>
